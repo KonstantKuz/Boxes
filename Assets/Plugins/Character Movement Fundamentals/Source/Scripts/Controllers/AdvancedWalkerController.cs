4a3e8c71d1f7a586f3b81a36d58be45367c67a72
KonstantKuz <stalkercatkuz@gmail.com>
2025-08-20 21:38:45 +0800

    update camera

--- Assets/Plugins/Character Movement Fundamentals/Source/Scripts/Controllers/AdvancedWalkerController.cs
+++ Assets/Plugins/Character Movement Fundamentals/Source/Scripts/Controllers/AdvancedWalkerController.cs
@@ -8,7 +8,7 @@ namespace CMF
 	//This controller is used as a basis for other controller types ('SidescrollerController');
 	//Custom movement input can be implemented by creating a new script that inherits 'AdvancedWalkerController' and overriding the 'CalculateMovementDirection' function;
 	public class AdvancedWalkerController : Controller {
-		
+
 		//References to attached components;
 		protected Transform tr;
 		protected Mover mover;
@@ -53,14 +53,14 @@ namespace CMF
 		public float gravity = 30f;
 		[Tooltip("How fast the character will slide down steep slopes.")]
 		public float slideGravity = 5f;
-		
+
 		//Acceptable slope angle limit;
 		public float slopeLimit = 80f;
 
 		[Tooltip("Whether to calculate and apply momentum relative to the controller's transform.")]
 		public bool useLocalMomentum = false;
 
-		//Enum describing basic controller states; 
+		//Enum describing basic controller states;
 		public enum ControllerState
 		{
 			Grounded,
@@ -69,12 +69,12 @@ namespace CMF
 			Rising,
 			Jumping
 		}
-		
+
 		ControllerState currentControllerState = ControllerState.Falling;
 
 		[Tooltip("Optional camera transform used for calculating movement direction. If assigned, character movement will take camera view into account.")]
 		public Transform cameraTransform;
-		
+
 		//Get references to all necessary components;
 		void Awake () {
 			mover = GetComponent<Mover>();
@@ -86,6 +86,11 @@ namespace CMF
 				Debug.LogWarning("No character input script has been attached to this gameobject", this.gameObject);
 
 			Setup();
+		}
+
+		public void SetCameraTransform(Transform target)
+		{
+			cameraTransform = target;
 		}
 
 		//This function is called right after Awake(); It can be overridden by inheriting scripts;
@@ -140,7 +145,7 @@ namespace CMF
 			Vector3 _velocity = Vector3.zero;
 			if(currentControllerState == ControllerState.Grounded)
 				_velocity = CalculateMovementVelocity();
-			
+
 			//If local momentum is used, transform momentum into world space first;
 			Vector3 _worldMomentum = momentum;
 			if(useLocalMomentum)
@@ -148,17 +153,17 @@ namespace CMF
 
 			//Add current momentum to velocity;
 			_velocity += _worldMomentum;
-			
+
 			//If player is grounded or sliding on a slope, extend mover's sensor range;
 			//This enables the player to walk up/down stairs and slopes without losing ground contact;
 			mover.SetExtendSensorRange(IsGrounded());
 
-			//Set mover velocity;		
+			//Set mover velocity;
 			mover.SetVelocity(_velocity);
 
 			//Store velocity for next frame;
 			savedVelocity = _velocity;
-		
+
 			//Save controller movement velocity;
 			savedMovementVelocity = CalculateMovementVelocity();
 
@@ -232,7 +237,7 @@ namespace CMF
 			bool _isRising = IsRisingOrFalling() && (VectorMath.GetDotProduct(GetMomentum(), tr.up) > 0f);
 			//Check if controller is sliding;
 			bool _isSliding = mover.IsGrounded() && IsGroundTooSteep();
-			
+
 			//Grounded;
 			if(currentControllerState == ControllerState.Grounded)
 			{
@@ -266,10 +271,10 @@ namespace CMF
 				}
 				return ControllerState.Falling;
 			}
-			
+
 			//Sliding;
 			if(currentControllerState == ControllerState.Sliding)
-			{	
+			{
 				if(_isRising){
 					OnGroundContactLost();
 					return ControllerState.Rising;
@@ -335,7 +340,7 @@ namespace CMF
 				}
 				return ControllerState.Jumping;
 			}
-			
+
 			return ControllerState.Falling;
 		}
 
@@ -392,7 +397,7 @@ namespace CMF
 					//Prevent unwanted accumulation of speed in the direction of the current momentum;
 					if(VectorMath.GetDotProduct(_movementVelocity, _horizontalMomentum.normalized) > 0f)
 						_movementVelocity = VectorMath.RemoveDotVector(_movementVelocity, _horizontalMomentum.normalized);
-					
+
 					//Lower air control slightly with a multiplier to add some 'weight' to any momentum applied to the controller;
 					float _airControlMultiplier = 0.25f;
 					_horizontalMomentum += _movementVelocity * Time.deltaTime * airControlRate * _airControlMultiplier;
@@ -425,7 +430,7 @@ namespace CMF
 			if(currentControllerState == ControllerState.Grounded)
 				_horizontalMomentum = VectorMath.IncrementVectorTowardTargetVector(_horizontalMomentum, groundFriction, Time.deltaTime, Vector3.zero);
 			else
-				_horizontalMomentum = VectorMath.IncrementVectorTowardTargetVector(_horizontalMomentum, airFriction, Time.deltaTime, Vector3.zero); 
+				_horizontalMomentum = VectorMath.IncrementVectorTowardTargetVector(_horizontalMomentum, airFriction, Time.deltaTime, Vector3.zero);
 
 			//Add horizontal and vertical momentum back together;
 			momentum = _horizontalMomentum + _verticalMomentum;
@@ -444,7 +449,7 @@ namespace CMF
 				Vector3 _slideDirection = Vector3.ProjectOnPlane(-tr.up, mover.GetGroundNormal()).normalized;
 				momentum += _slideDirection * slideGravity * Time.deltaTime;
 			}
-			
+
 			//If controller is jumping, override vertical velocity with jumpSpeed;
 			if(currentControllerState == ControllerState.Jumping)
 			{
@@ -505,7 +510,7 @@ namespace CMF
 				if(_projectedMomentum.sqrMagnitude >= _velocity.sqrMagnitude && _dot > 0f)
 					_velocity = Vector3.zero;
 				else if(_dot > 0f)
-					_velocity -= _projectedMomentum;	
+					_velocity -= _projectedMomentum;
 			}
 
 			//Add movement velocity to momentum;
@@ -528,7 +533,7 @@ namespace CMF
 
 				OnLand(_collisionVelocity);
 			}
-				
+
 		}
 
 		//This function is called when the controller has collided with a ceiling while jumping or moving upwards;
@@ -612,7 +617,7 @@ namespace CMF
 			if(useLocalMomentum)
 				momentum = tr.localToWorldMatrix * momentum;
 
-			momentum += _momentum;	
+			momentum += _momentum;
 
 			if(useLocalMomentum)
 				momentum = tr.worldToLocalMatrix * momentum;
